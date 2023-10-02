5d76c5c03923086e8e90ae51d975eefe712eaf32
Alexander Repeta <alexander.repeta@gmail.com>
2023-10-02 21:30:06 +0300

    add lesson 01 examples

---
+++ js/01-vars.js
@@ -0,0 +1,20 @@
+/*
+  - Объявление переменных с let и const
+  - Перевизначення значення
+  - Найменування змінних
+  - Вибір між const і let
+ */
+
+// const age = ;
+// const totalPrice = ;
+// const userName = ;
+// const message = ;
+// const isOpen = ;
+// const shouldConfirm = ;
+
+/*
+  Оператор typeof
+ */
+
+// const type = ;
+// console.log(type);
---
+++ js/04-strings.js
@@ -0,0 +1,39 @@
+/*
+  Конкатенация строк
+ */
+// const firstName = "Chelsy";
+// const lastName = "Emerald";
+// const fullName = ;
+// console.log(fullName);
+
+/*
+  Шаблонные строки (template strings)
+ */
+
+// const quantity = 15;
+// const orderMsg = "You ordered quantity bots";
+// console.log(orderMsg);
+
+/*
+  Длина строки, свойство length
+ */
+// const message = "This string is 28 chars long";
+// console.log();
+
+/*
+ Напиши скрипт который выведет строку в формате:
+ "Гость x y поселяется в z номер q",
+ подставив вместо x y z q значения переменных
+ */
+
+// const firstName = "Chelsy";
+// const lastName = "Emerald";
+// const room = 716;
+// const type = "VIP";
+
+// const welcomeMsg = ;
+// console.log(welcomeMsg);
+
+/*
+  Незмінність рядків
+*/
