c51df95bc4d70e2313a35615da96ebc2e98c7051
Alexander Repeta <alexander.repeta@gmail.com>
2023-11-30 12:16:59 +0200

    add module 10 lesson 2

---
+++ uk/module-10/lesson-02/solution/02-promisification.js
@@ -0,0 +1,52 @@
+/*
+ * Промісифікація:
+ * - Проблема доступу до результату проміса з колбеком
+ * - Функція, яка повертає проміс
+ */
+
+const makeOrder = (dish) => {
+  return new Promise((resolve, reject) => {
+    const passed = Math.random() > 0.5;
+
+    setTimeout(() => {
+      if (passed) {
+        resolve(`✅ Ваше замовлення: ${dish}`);
+      }
+
+      reject("❌ Упс, у нас закінчилися продукти");
+    }, 1000);
+  });
+};
+
+makeOrder("пиріжок")
+  .then((result) => {
+    console.log("onMakeOrderSuccess");
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log("onMakeOrderError");
+    console.log(error);
+  });
+
+/*
+ * Промісифікація «синхронних» функцій
+ * - Promise.resolve()
+ * - Promise.reject()
+ */
+
+const prepareDish = (dish) => {
+  const passed = Math.random() > 0.5;
+  return passed
+    ? Promise.resolve(`✅ Ваше замовлення: ${dish}`)
+    : Promise.reject("❌ Упс, у нас закінчилися продукти");
+};
+
+makeOrder("пиріжок")
+  .then((result) => {
+    console.log("onMakeOrderSuccess");
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log("onMakeOrderError");
+    console.log(error);
+  });
