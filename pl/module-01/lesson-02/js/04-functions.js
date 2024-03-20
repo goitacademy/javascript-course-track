c7a3ce744b62a248258af7f7e00979b68dcc5ac6
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-03-20 15:29:12 +0100

    PL_04-functions.js
    pl translation added

--- pl/module-01/lesson-02/js/04-functions.js
+++ pl/module-01/lesson-02/js/04-functions.js
@@ -1,9 +1,9 @@
 /**
- * Функции
+ * Funkcje
  *
- * - Оголошення та виклик функції
- * - Параметри та аргументи
- * - Повернення значення
+ * - Deklarowanie i wywoływanie funkcji
+ * - Parametry i argumenty
+ * - Zwracanie wartości
  */
 
 // console.log("Before fnA execution");
@@ -19,15 +19,15 @@
 // console.log("After fnC execution");
 
 /**
- * Напиши функцію calcBMI(weight, height) яка розраховує та повертає індекс
- * маси тіла людини. Для цього необхідно розділити вагу на
- * квадрат висоти.
+ * Napisz funkcję calcBMI(weight, height) która oblicza i zwraca wskaźnik
+ * masy ciała człowieka. Aby to zrobić, podziel wagę przez
+ * kwadrat wzrostu.
  *
- * Вага та висота будуть спеціально передані як рядки. Не цілі числа можуть
- * бути задані у вигляді 24.7 або 24,7, тобто як роздільник дробової
- * частини може бути кома.
+ * Waga i wzrost będą specjalnie przekazywane jako ciągi znaków. Liczby niecałkowite mogą
+ * być podane jako 24.7 lub 24,7, tzn. przecinek może być użyty jako
+ * separator części ułamkowej.
  *
- * Індекс маси тіла необхідно округлити до однієї цифри після коми.
+ * Wskaźnik masy ciała należy zaokrąglić do jednego miejsca po przecinku.
  */
 
 // const bmi = calcBMI("88,3", "1.75");
