36141d0fa4549b4c63c857b9ae6eb8896b007608
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-03-26 10:43:28 +0100

    pl translation added

--- pl/module-06/lesson-02/js/05-storage.js
+++ pl/module-06/lesson-02/js/05-storage.js
@@ -1,12 +1,12 @@
 /**
- * Напиши клас Storage який створює об'єкти для керування складом товарів.
- * При виклику отримуватиме один аргумент - початковий масив товарів і
- * записуватиме його властивість items.
+ * Napisz klasę Storage, która tworzy obiekty do zarządzania magazynem towarów.
+ * Po wywołaniu będzie ona pobierać jeden argument - początkową tablicę towarów i
+ * zapisywać jej właściwość items.
  *
- * Додай методи класу:
- * - getItems() - повертає масив товарів.
- * - addItem(item) - отримує новий товар і додає його до поточних.
- * - removeItem(item) - отримує товар і, якщо він є, видаляє його з поточних.
+ * Dodaj metody klasy:
+ * - getItems() - zwraca tablicę elementów.
+ * - addItem(item) - pobiera nowy element i dodaje go do bieżących.
+ * - removeItem(item) - pobiera element i, jeśli istnieje, usuwa go z bieżących.
  */
 const storage = new Storage(["🍎", "🍋", "🍇", "🍑"]);
 
