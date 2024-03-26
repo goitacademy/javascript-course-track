592b47cffab83278e280c9be246163e822288567
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-03-26 10:53:26 +0100

    pl translation added

--- pl/module-07/lesson-01/js/02-properties.js
+++ pl/module-07/lesson-01/js/02-properties.js
@@ -1,7 +1,7 @@
 /*
- * Властивості елемента (hero)
- * - Зображення
- * - Текст та textContent
+ * Właściwości elementu  (hero)
+ * - Obraz
+ * - Tekst i textContent
  * https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480
  */
 
@@ -10,14 +10,14 @@ const imageEl = document.querySelector('.hero__image');
 const heroTitleEl = document.querySelector('.hero__title');
 
 /*
- * Атрибути
- * - get(ім'я-атрибута)
- * - set(ім'я-атрибута)
- * - remove(ім'я-атрибута)
- * - has(ім'я-атрибута)
+ * Atrybuty
+ * - get(nazwa-atrybutu)
+ * - set(nazwa-atrybutu)
+ * - remove(nazwa-atrybutu)
+ * - has(nazwa-atrybutu)
  */
 
 /*
- * Data-атрибути
+ * Atrybuty danych
  */
 const actions = document.querySelectorAll('.actions button');
