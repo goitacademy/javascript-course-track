203d62b3cfa77759e0c614d1e4aed0ffdc76fb62
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-03-26 15:07:48 +0100

    pl translation added

--- pl/module-12/lesson-02/index.js
+++ pl/module-12/lesson-02/index.js
@@ -1,14 +1,14 @@
 /**
- * Отримуємо ключ https://newsapi.org/
- * Запити робитимемо на http://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1
+ * Pobieramy klucz https://newsapi.org/
+ * Zapytania będziemy wysyłać do http://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1
  *
- * Пагінація: номер групи та кількість елементів групи
- * - Завантажуємо статті при самітті форми
- * - Завантажуємо статті при натисканні на кнопку «Завантажити ще»
- * - Оновлюємо групу в параметрах запиту
- * - Рендерим статті
- * - Скидання значення при пошуку за новим критерієм
- * - Показуємо спинер поки йде запит
+ * Paginacja: numer grupy i liczba elementów grupy
+ * - Ładujemy artykuły po przesłaniu formularza
+ * - Ładujemy artykuły po kliknięciu przycisku "Załaduj więcej"
+ * - Aktualizujemy grupę w parametrach zapytania
+ * - Renderujemy artykuły
+ * - Resetowanie wartości podczas wyszukiwania według nowego kryterium
+ * - Pokazujemy spinner podczas wykonywania zapytania
  */
 
 const searchForm = document.querySelector(".search-form");
