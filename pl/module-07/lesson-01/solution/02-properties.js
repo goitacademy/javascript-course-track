a283bd857b692fff75bcc575378441853f779d33
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-03-26 11:00:13 +0100

    pl translation added

--- pl/module-07/lesson-01/solution/02-properties.js
+++ pl/module-07/lesson-01/solution/02-properties.js
@@ -1,7 +1,7 @@
 /*
- * Властивості елемента (hero)
- * - Зображення
- * - Текст та textContent
+ * Właściwości elementu  (hero)
+ * - Obraz
+ * - Tekst i textContent
  * https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480
  */
 
@@ -9,18 +9,18 @@ const imageEl = document.querySelector('.hero__image');
 console.log('imageEl', imageEl);
 imageEl.src =
   'https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480';
-imageEl.alt = 'Новий котик';
+imageEl.alt = 'Nowy kociak';
 
 const heroTitleEl = document.querySelector('.hero__title');
 console.log('heroTitleEl', heroTitleEl);
-heroTitleEl.textContent = 'Це новий текст!';
+heroTitleEl.textContent = 'To jest nowy tekst!';
 
 /*
- * Атрибути
- * - get(ім'я-атрибута)
- * - set(ім'я-атрибута)
- * - remove(ім'я-атрибута)
- * - has(ім'я-атрибута)
+ * Atrybuty
+ * - get(nazwa-atrybutu)
+ * - set(nazwa-atrybutu)
+ * - remove(nazwa-atrybutu)
+ * - has(nazwa-atrybutu)
  */
 
 console.log(imageEl.getAttribute('src'));
@@ -28,7 +28,7 @@ console.log(imageEl.src);
 console.log(imageEl.hasAttribute('src'));
 
 /*
- * Data-атрибути
+ * Atrybuty danych
  */
 const actions = document.querySelectorAll('.actions button');
 console.log(actions);
