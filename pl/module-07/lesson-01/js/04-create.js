d27178a1c1a3fa8f7a3620d615af8fdd73c5f6c0
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-03-26 10:55:02 +0100

    pl translation added

--- pl/module-07/lesson-01/js/04-create.js
+++ pl/module-07/lesson-01/js/04-create.js
@@ -1,14 +1,14 @@
 /*
- * Створення та додавання елементів
+ * Tworzenie i dodawanie elementów
  */
 
 /*
- * Створюємо заголовок
+ * Tworzymy nagłówek
  */
 const titleEl = document.createElement('h1');
 
 /*
- * Створюємо зображення
+ * Tworzymy obraz
  * https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg
  * valais-alpine-mountains-glacier
  */
@@ -17,6 +17,6 @@ const imageEl = document.createElement('img');
 const heroEl = document.querySelector('.hero');
 
 /*
- * Створюємо та додаємо новий пункт меню
+ * Tworzymy i dodajemy nową pozycję menu
  */
 const navItemEl = document.createElement('li');
