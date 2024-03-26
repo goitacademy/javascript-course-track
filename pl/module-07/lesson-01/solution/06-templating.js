e4052d3526b8228dba518699edff4d71961d6a00
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-03-26 11:04:47 +0100

    pl translation added

--- pl/module-07/lesson-01/solution/06-templating.js
+++ pl/module-07/lesson-01/solution/06-templating.js
@@ -1,18 +1,18 @@
 /*
- * Властивість innerHTML
- * - зчитування
- * - запис
+ * Właściwość innerHTML
+ * - odczyt
+ * - zapis
  */
 
 const titleEl = document.querySelector('.title');
-titleEl.innerHTML = '<a href="">Це посилання)</a>';
+titleEl.innerHTML = '<a href="">To jest link)</a>';
 titleEl.innerHTML = '';
 
 /*
- * Вставка розмітки за допомогою insertAdjacentHTML()
+ * Wstawianie znaczników za pomocą insertAdjacentHTML()
  */
 
 titleEl.insertAdjacentHTML(
   'beforeend',
-  '<a href="" class="title__link">Це посилання!</a>',
+  '<a href="" class="title__link">To jest link!</a>',
 );
