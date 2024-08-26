5a59768f7b74cce9723118154300ff3ca0a446b1
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-08-26 17:59:28 +0300

    Update 05-tasks.js

--- ro/module-03/lesson-01/js/05-tasks.js
+++ ro/module-03/lesson-01/js/05-tasks.js
@@ -1,32 +1,33 @@
 /**
- * Напиши скрипт, який підраховує суму всіх парних чисел у масиві.
+ * Scrie un script care calculează suma tuturor numerelor pare dintr-un array.
  */
 
 const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
 let total = 0;
 
 /**
- * Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача.
- * У змінних names та phones зберігаються рядки імен та телефонних номерів,
- * розділені комами. Порядковий номер імен та телефонів у рядках вказують на
- * відповідність. Кількість імен та телефонів гарантовано однакова.
+ * Scrie un script care afișează în consolă numele și numărul de telefon al utilizatorului.
+ * Variabilele names și phones conțin șiruri de caractere cu numele și numerele de telefon
+ * separate prin virgule. Fiecare nume din `names` corespunde unui număr de telefon din 
+ * `phones`, iar ordinea lor este aceeași. Numărul de nume și numere de telefon va fi 
+ * întotdeauna egal.
  */
 
 const names = "Jacob,William,Solomon,Artemis";
 const phones = "89001234567,89001112233,890055566377,890055566300";
 
 /**
- * Напиши скрипт, який виводить у консоль усі слова рядка
- * крім першого і останнього. Результуючий рядок не повинен починатися
- * або закінчуватися символ пробілу.
- * Скрипт повинен працювати для будь-якого рядка.
+ * Scrie un script care afișează în consolă toate cuvintele dintr-un șir de caractere,
+ * cu excepția primului și ultimului cuvânt. Șirul rezultat nu trebuie să înceapă
+ * sau să se termine cu spațiu.
+ * Scriptul trebuie să funcționeze corect pentru orice șir de caractere.
  */
 
 const string = "Welcome to the future";
 
 /**
- * Напиши скрипт пошуку найменшого числа у масиві. Код повинен працювати
- * для будь-якого масиву чисел. Використовуй цикл для розв'язання задачі.
+ * Scrie un script pentru a găsi cel mai mic număr dintr-un array. Codul trebuie să funcționeze
+ * pentru orice array de numere. Folosește o buclă pentru a rezolva problema.
  */
 
 const values = [2, 17, 94, 1, 23, 37];
