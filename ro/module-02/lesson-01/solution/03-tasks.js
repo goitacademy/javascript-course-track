5c9213f395221b5d7191f218bdf6c1c20b81f8fa
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-08-26 15:47:42 +0300

    Update 03-tasks.js

--- ro/module-02/lesson-01/solution/03-tasks.js
+++ ro/module-02/lesson-01/solution/03-tasks.js
@@ -1,7 +1,7 @@
 /**
- * Напиши скрипт для відображення годин та хвилин у консолі браузера у вигляді
- * рядка формату "14 г. 26 хв.". Якщо значення змінної minutes дорівнює 0,
- * то виводь рядок "14 г.", без хвилин.
+ * Scrie un script pentru a afișa orele și minutele în consola browser-ului
+ * sub formă de șir de caractere în formatul "14 h 26 min". Dacă valoarea
+ * variabilei minutes este 0, atunci afișează șirul "14 h", fără minute.
  */
 
 const hours = 14;
@@ -9,21 +9,20 @@ const minutes = 26;
 let timestring;
 
 if (minutes > 0) {
-  timestring = `${hours} г. ${minutes} хв.`;
+  timestring = `${hours} h ${minutes} min`;
 } else {
-  timestring = `${hours} г.`;
+  timestring = `${hours} h`;
 }
 console.log(timestring);
 
 /**
- * Напиши скрипт для відображення часу дедлайну здачі проекту. Використовуй if...else.
+ * Scrie un script pentru a afișa timpul rămas până la deadline-ul proiectului. Folosește if...else.
  *
- * Якщо до дедлайну 0 днів - виведи рядок "Today"
- * Якщо до дедлайну 1 день - виведи рядок "Tomorrow"
- * Якщо до дедлайну 2 дні - виведи рядок "Overmorrow"
- * Якщо до дедлайну 3+ днів - виведи рядок "Date in the future"
+ * Dacă până la deadline mai sunt 0 zile - afișează șirul "Today"
+ * Dacă mai este 1 zi - afișează șirul "Tomorrow"
+ * Dacă mai sunt 2 zile - afișează șirul "Overmorrow"
+ * Dacă mai sunt 3+ zile - afișează șirul "Date in the future"
  */
-
 const daysUntilDeadline = 5;
 
 if (daysUntilDeadline === 0) {
