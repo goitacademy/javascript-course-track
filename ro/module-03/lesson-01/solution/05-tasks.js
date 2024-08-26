fe7210ee9eb0d22e926200baede87fb03ba0e861
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-08-26 17:58:05 +0300

    Update 05-tasks.js

--- ro/module-03/lesson-01/solution/05-tasks.js
+++ ro/module-03/lesson-01/solution/05-tasks.js
@@ -1,5 +1,5 @@
 /**
- * Напиши скрипт, який підраховує суму всіх парних чисел у масиві.
+ * Scrie un script care calculează suma tuturor numerelor pare dintr-un array.
  */
 
 const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
@@ -18,20 +18,21 @@ for (const number of numbers) {
 console.log("Total: ", total);
 
 /**
- * Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача.
- * У змінних names та phones зберігаються рядки імен та телефонних номерів,
- * розділені комами. Порядковий номер імен та телефонів у рядках вказують на
- * відповідність. Кількість імен та телефонів гарантовано однакова.
+ * Scrie un script care afișează în consolă numele și numărul de telefon al utilizatorului.
+ * Variabilele names și phones conțin șiruri de caractere cu numele și numerele de telefon
+ * separate prin virgule. Fiecare nume din `names` corespunde unui număr de telefon din 
+ * `phones`, iar ordinea lor este aceeași. Numărul de nume și numere de telefon va fi 
+ * întotdeauna egal.
  */
 
 const names = "Jacob,William,Solomon,Artemis";
 const phones = "89001234567,89001112233,890055566377,890055566300";
 
 /**
- * Напиши скрипт, який виводить у консоль усі слова рядка
- * крім першого і останнього. Результуючий рядок не повинен починатися
- * або закінчуватися символ пробілу.
- * Скрипт повинен працювати для будь-якого рядка.
+ * Scrie un script care afișează în consolă toate cuvintele dintr-un șir de caractere,
+ * cu excepția primului și ultimului cuvânt. Șirul rezultat nu trebuie să înceapă
+ * sau să se termine cu spațiu.
+ * Scriptul trebuie să funcționeze corect pentru orice șir de caractere.
  */
 
 const string = "Welcome to the future";
