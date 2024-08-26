351451ce9a56bf11b1a329ab39c08141bf52cef4
goit-i18n <84774169+goit-i18n@users.noreply.github.com>
2024-08-26 17:02:50 +0300

    Update 02-scope.js

--- ro/module-03/lesson-02/solution/02-scope.js
+++ ro/module-03/lesson-02/solution/02-scope.js
@@ -1,9 +1,8 @@
 /**
- * - Глобальна область видимості
- * - Блочна область видимості
- * - Пошук за ланцюжком областей видимості
+ * - Domeniul de vizibilitate global
+ * - Domeniul de vizibilitate blocked
+ * - Căutarea prin lanțul de domenii de vizibilitate
  */
-
 const globalValue = 10;
 
 console.log(globalValue); // 10
@@ -24,10 +23,11 @@ function foo() {
   }
 }
 
-// ❌ Помилка! Змінна a не доступна в цій області видимості
+// ❌ Eroare! Variabila a nu poate fi accesată în acest domeniu de vizibilitate
 console.log(a);
 
 for (let i = 0; i < 3; i += 1) {
-  // ❌ Помилка! Змінна a не доступна в цій області видимості
+  // ❌ Eroare! Variabila a nu poate fi accesată în acest domeniu de vizibilitate
   console.log(a);
 }
+
